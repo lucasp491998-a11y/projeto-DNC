1829d88d762895a36350aa4ef9fa6d65f5f93cf6
lucas alexandre pontes <lucasp491998@gmail.com>
2026-05-03 17:12:25 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var setaDireita = window.document.getElementById("seta-direita");
+var leonardo = window.document.getElementById("leonardo");
+var bruna = window.document.getElementById("bruna");
+var samantha = window.document.getElementById("samantha");
+var setaEsquerda = window.document.getElementById("seta-esquerda");
+
+function RolarParaDireita() {
+ leonardo.style = "display:none"
+ samantha.style = "display:flex"
+ setaDireita.style = "display:none"
+ setaEsquerda.style = "display:flex; margin-top: 115px; margin-left: 10px;"
+}
+
+ function RolarParaEsquerda() {
+    samantha.style = "display:none"
+    leonardo.style = "display:flex"
+    setaEsquerda.style = "display:none"
+    setaDireita.style = "display:flex; margin-left: 10px;"
+}
